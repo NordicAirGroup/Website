f4b456ffdff271668b6f75cbb65c997d9483f096
NordicAirGroup <info@nordicairgroup.com>
2023-12-17 19:36:57 +0100

    w

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
+require("dotenv").config({ path: __dirname + "/.env" });
 const express = require("express");
 const nodemailer = require("nodemailer");
 const app = express();
 const cors = require("cors");
-require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
@@ -17,13 +17,11 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-console.log("SendGrid API Key:", process.env.SENDGRID_API_KEY);
-
 app.post("/send", (req, res) => {
   const { name, email, message } = req.body;
   const mailOptions = {
     from: "info@nordicairgroup.com",
-    to: "info@nordicairgroup.com",
+    to: "info@nordicairgroup.com, joar@nordicairgroup.com, ersu@nordicairgroup.com",
     subject: "New Contact Form Submission",
     text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
   };
