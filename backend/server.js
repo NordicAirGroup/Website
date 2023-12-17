59b3dc0530ae0145ef46c3fee754ad354634b347
NordicAirGroup <info@nordicairgroup.com>
2023-12-17 20:13:20 +0100

    w

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,10 @@ app.post("/send", (req, res) => {
   });
 });
 
+app.get("/", (req, res) => {
+  res.send("Backend server is running!");
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
