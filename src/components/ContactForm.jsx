8d6a778f494a68e03427b43e2b6db7977fdc1ee3
NordicAirGroup <info@nordicairgroup.com>
2023-12-18 12:30:39 +0100

    w

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -20,7 +20,14 @@ function ContactForm() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (!formData.name || !formData.email || !formData.message) {
+      alert("Please fill in all fields.");
+      return;
+    }
+    
     try {
+      
       const response = await fetch("https://still-river-91998-c2c541cfc191.herokuapp.com/send", {
         method: "POST",
         headers: {
