5c48d7403389cd71e4b6360d4e2a684578d6e704
NordicAirGroup <info@nordicairgroup.com>
2023-12-19 16:40:29 +0100

    cooldown

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const app = express();
 const cors = require("cors");
-const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:3001";
 
 app.use(cors());
 app.use(express.json());
@@ -11,15 +10,39 @@ app.use(express.json());
 const transporter = nodemailer.createTransport({
   host: "smtp.sendgrid.net",
   port: 587,
-  secure: false, // Note: 'secure' is false because we are using port 587 (not 465)
+  secure: false,
   auth: {
-    user: "apikey", // This should remain the string 'apikey'
+    user: "apikey",
     pass: process.env.SENDGRID_API_KEY,
   },
 });
 
+const messageQuota = {}; // Object to keep track of user message counts and timestamps
+
 app.post("/send", (req, res) => {
   const { name, email, message } = req.body;
+
+  const currentTime = new Date().getTime();
+  const dayInMillis = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+
+  if (!messageQuota[email]) {
+    messageQuota[email] = { count: 1, timestamp: currentTime };
+  } else {
+    if (currentTime - messageQuota[email].timestamp < dayInMillis) {
+      if (messageQuota[email].count >= 2) {
+        return res
+          .status(429)
+          .json({
+            status: "error",
+            message: "Message quota exceeded. Please wait 24 hours.",
+          });
+      }
+      messageQuota[email].count++;
+    } else {
+      messageQuota[email] = { count: 1, timestamp: currentTime };
+    }
+  }
+
   const mailOptions = {
     from: "info@nordicairgroup.com",
     to: "info@nordicairgroup.com, joar@nordicairgroup.com, ersu@nordicairgroup.com",
@@ -30,10 +53,10 @@ app.post("/send", (req, res) => {
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log(error);
-      res.status(500).json({ status: "error", message: error.message }); // Send back a JSON with error details
+      res.status(500).json({ status: "error", message: error.message });
     } else {
       console.log("Email sent: " + info.response);
-      res.json({ status: "success", message: "Email sent successfully!" }); // Send back a JSON with success message
+      res.json({ status: "success", message: "Email sent successfully!" });
     }
   });
 });
