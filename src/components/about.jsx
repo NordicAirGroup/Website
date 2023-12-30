c66439f077fe8c77daff82be560fd4d02a286508
NordicAirGroup <info@nordicairgroup.com>
2023-12-30 13:30:36 +0100

    w

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -40,31 +40,28 @@ const About = () => {
               <h1>Our Story</h1>
               <p>
                 Joar and Ersu had just finished their first lecture, during
-                which Ersu shared his deep passion for aviation and his
-                aspirations to become a pilot or aviation broker. Joar, recently
-                landing a role in appointment setting, saw an opportunity. He
-                suggested Ersu could gain valuable experience and connections
-                through similar work. Initially neutral, Ersu warmed up to the
-                idea as they explored it further, realizing they could add
-                genuine value to the field. Thus, a dream took flight: entering
+                which Ersu shared his plan for his aspirations to become a pilot
+                or aviation broker. Joar, recently landing a role in appointment
+                setting, saw an opportunity. He suggested Ersu could gain
+                valuable experience and connections through similar work.
+                Initially neutral, Ersu warmed up to the idea as they explored
+                it further, realizing they could add genuine value to the field.
+                They decided to team up and thus, a dream took flight: entering
                 the private aviation industry with a straightforward service
                 connecting buyer's and seller's brokers. Our role is simple yet
                 impactful – facilitating connections when a strong fit is
                 identified, saving time and simplifying the process for both
-                parties. Aiming to give substantially more value, than they
-                take.
+                parties.
                 <br />
                 <br />
-                What drives us? Firstly, the thrill of making our mark in the
-                prestigious world of private aviation. Our shared passions –
-                aviation, sales, and entrepreneurship – make this venture
-                irresistible. But more importantly, we're here to contribute
-                meaningfully, solving problems and putting in the hard work. We
-                stand as the future of the world, determined to set ourselves
-                apart. While many in our generation might hesitate to go the
-                extra mile, we are committed to conquer the challenges that lie
-                ahead – and, on a lighter note, we're also looking to fund our
-                pilot licenses!
+                What drives us? Our shared passions – aviation, sales, and
+                entrepreneurship – make this venture irresistible. But more
+                importantly, we're here to contribute meaningfully, solving
+                problems and putting in the hard work. We stand as the future of
+                the world, determined to set ourselves apart. While many in our
+                generation might hesitate to go the extra mile, we are committed
+                to conquer the challenges that lie ahead – and, on a lighter
+                note, we're also looking to fund our pilot licenses!
               </p>
             </div>
           </div>
