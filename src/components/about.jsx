3ea802df86c0edd66cab583510cbea0af082a315
NordicAirGroup <info@nordicairgroup.com>
2023-12-30 12:54:07 +0100

    w

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Joar from "./Joar";
 import Ersu from "./Ersu";
 import "../styles/about.css";
+import "../styles/ourstory.css";
+import JetImage from "../assets/JetImage.jpg";
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 
@@ -15,9 +17,53 @@ const About = () => {
   }, [location]);
 
   return (
-    <div id="about" className="about-container">
-      <Joar />
-      <Ersu />
+    <div>
+      <div id="about" className="about-container">
+        <Joar />
+        <Ersu />
+      </div>
+      <div className="ourstory-container">
+        <div className="ourstory-content">
+          <div className="jetimage-container">
+            <img src={JetImage} alt="JetImage" />
+          </div>
+          <div className="ourstory-card-container">
+            <div className="ourstory-card">
+              <h1>Our Story</h1>
+              <p>
+                Joar and Ersu had just finished their first lecture, during
+                which Ersu shared his deep passion for aviation and his
+                aspirations to become a pilot or aviation broker. Joar, recently
+                landing a role in appointment setting, saw an opportunity. He
+                suggested Ersu could gain valuable experience and connections
+                through similar work. Initially neutral, Ersu warmed up to the
+                idea as they explored it further, realizing they could add
+                genuine value to the field. Thus, a dream took flight: entering
+                the private aviation industry with a straightforward service
+                connecting buyer's and seller's brokers. Our role is simple yet
+                impactful – facilitating connections when a strong fit is
+                identified, saving time and simplifying the process for both
+                parties. Aiming to give substantially more value, than they
+                take.
+                <br />
+                <br />
+                What drives us? Firstly, the thrill of making our mark in the
+                prestigious world of private aviation. Our shared passions –
+                aviation, sales, and entrepreneurship – make this venture
+                irresistible. But more importantly, we're here to contribute
+                meaningfully, solving problems and putting in the hard work. We
+                stand as the future of the world, determined to set ourselves
+                apart. While many in our generation might hesitate to go the
+                extra mile, we are committed to conquer the challenges that lie
+                ahead – and, on a lighter note, we're also looking to fund our
+                pilot licenses!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="ourstory-color"></div>
+        <div className="ourstory-color2"></div>
+      </div>
     </div>
   );
 };
